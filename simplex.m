72bb08d7c7d6a964d9c614b74f338ed84bcdf99d
Marc Hauptmann <marc.hauptmann@stud.uni-hannover.de>
2014-03-18 13:06:24 +0100

    implemented simplex method
    simple version of the simplex method dealing with equality constraints

---
+++ simplex.m
@@ -0,0 +1,44 @@
+function x = simplex(c, A, b)
+%SIMPLEX Simplex method
+%   x = SIMPLEX(c, A, b)
+%
+%   solves the problem
+%   $$ \min_x c^Tx $$
+%   s.t. Ax = b
+%        x >= 0
+%
+
+    [m, n] = size(A);
+    BIndices = 1:m;
+    
+    while 1
+        NIndices = setdiff(1:n, BIndices);
+
+        B = A(:,BIndices);
+        N = A(:,NIndices);
+
+        x = zeros(n, 1);
+        s = zeros(n,1);
+
+        x(BIndices) = B\b;
+
+        lambda = B'\c(BIndices);
+        s(NIndices) = c(NIndices) - N'*lambda;
+
+        if all(s>=0)
+            fprintf('Lösung gefunden');
+            return;
+        end
+
+        [sq, q] = min(s);
+
+        d = B\A(:,q);
+
+        [xqp, p] = min(x(BIndices)./d);
+
+        x(BIndices) = x(BIndices) - d*xqp;
+        x(q) = xqp;
+        
+        BIndices = [setdiff(BIndices, p), q];
+    end
+end
